1c6a9086f1620965135952e6b3c7e074c245de3f
Emilio <esc00019@red.ujaen.es>
2018-05-07 17:16:27 +0200

    some css ajust

--- src/app/components/form-owner-info/form-owner-info.component.ts
+++ src/app/components/form-owner-info/form-owner-info.component.ts
@@ -2,14 +2,20 @@
  * @author Emilio Sánchez Catalán <esc00019@gmail>
  * Purpose: form with owner information for upload a home
  */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'alo-form-owner-info',
   templateUrl: './form-owner-info.component.html',
   styleUrls: ['./form-owner-info.component.css']
 })
-export class FormOwnerInfoComponent {
+export class FormOwnerInfoComponent implements OnInit {
 
   constructor() { }
+
+  ngOnInit() {
+    $('.selectpicker').selectpicker();
+  }
 }
--- src/app/components/form-std-info/form-std-info.component.ts
+++ src/app/components/form-std-info/form-std-info.component.ts
@@ -2,14 +2,20 @@
  * @author Emilio Sánchez Catalán <esc00019@gmail>
  * Purpose: form with owner information for upload a home
  */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'alo-form-std-info',
   templateUrl: './form-std-info.component.html',
   styleUrls: ['./form-std-info.component.css']
 })
-export class FormStdInfoComponent {
+export class FormStdInfoComponent implements OnInit {
 
   constructor() { }
+
+  ngOnInit() {
+    $('.selectpicker').selectpicker();
+  }
 }
